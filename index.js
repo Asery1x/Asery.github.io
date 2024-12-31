d7dd9e9f04cf432443905e0983b28b8ebed5f405
Asery <135715004+Asery1x@users.noreply.github.com>
2024-12-31 12:31:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const faqQuestions = document.querySelectorAll('.faq-question');
+faqQuestions.forEach((question) => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+    const isExpanded = answer.classList.contains('show');
+
+    if (isExpanded) {
+      answer.style.maxHeight = '0';
+      answer.classList.remove('show');
+    } else {
+      answer.classList.add('show');
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    }
+  });
+});
